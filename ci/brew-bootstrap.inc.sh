d45d3af25a4fa3d3c5fb3450b46e61ba97cfaf47
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-10 20:16:35 +0200

    fixup head

--- ci/brew-bootstrap.inc.sh
+++ ci/brew-bootstrap.inc.sh
@@ -21,7 +21,7 @@ case $(uname -s) in
     Darwin)
         if [[ "${HAS_BREW_2}" = "true" ]]; then
             echo_do "brew: Updating homebrew..."
-            brew update
+            brew update >/dev/null
             echo_done
         else
             echo_do "brew: Installing homebrew..."
@@ -41,7 +41,7 @@ case $(uname -s) in
     Linux)
         if [[ "${HAS_BREW_2}" = "true" ]]; then
             echo_do "brew: Updating linuxbrew..."
-            brew update
+            brew update >/dev/null
             echo_done
         else
             echo_do "brew: Installing linuxbrew..."
